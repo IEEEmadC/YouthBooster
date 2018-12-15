4f5993e1627c6a65d1955bcad6f2d3aba3844bb5
khalil mejri <khalil.mejri.39566@gmail.com>
2018-12-15 11:46:21 +0100

    add comments

--- src/pages/filter-items/filter-items.ts
+++ src/pages/filter-items/filter-items.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { BrowseProjectsPage} from "../browse-projects/browse-projects";
+import { BrowseProjectsPage } from "../browse-projects/browse-projects";
 
 
 @IonicPage()
@@ -10,53 +10,48 @@ import { BrowseProjectsPage} from "../browse-projects/browse-projects";
 })
 export class FilterItemsPage {
 
-       PassedFilteredItems:any;
+  PassedFilteredItems: any;
 
-        constructor(private navCtrl: NavController, public navParams: NavParams) {
-           this.PassedFilteredItems = this.navParams.get('projects');
-           console.log(this.PassedFilteredItems);
-         }
-
-
-         filter(item){
-
-                 this.PassedFilteredItems  = this.sortByKey( this.PassedFilteredItems ,item);
-
-                   this.navCtrl.setRoot(BrowseProjectsPage,{'projects': this.PassedFilteredItems,'filter' : item });
-         }
+  constructor(private navCtrl: NavController, public navParams: NavParams) {
+    this.PassedFilteredItems = this.navParams.get('projects');
+    console.log(this.PassedFilteredItems);
+  }
 
 
-                      public   sortByKey(array, key) {
-                return array.sort(function (a, b) {
-                 var x,y;
-                 var parts1,parts2,atime,btime;
+  filter(item) {
 
-                    if((key=='likes')||(key=='views'))
-                    {
-                     x = a[key];
-                     y = b[key];
-                    }
-                    else if((key=='newest')||(key=='oldest'))
-                    {
-                      atime=new Date(a['timestamp']);
-                      btime=new Date(b['timestamp']);
-                      atime=atime.getFullYear()+"/"+atime.getMonth()+"/"+atime.getDate();
-                      btime=btime.getFullYear()+"/"+btime.getMonth()+"/"+btime.getDate();
+    this.PassedFilteredItems = this.sortByKey(this.PassedFilteredItems, item);
 
-                      parts1=atime.split("/");
-                      parts2=btime.split("/");
-                      y = new Date(parts1[2], parts1[1], parts1[0]);
-                      x = new Date(parts2[2], parts2[1], parts2[0]);
-
-                    }
-                    if(key=='oldest')
-                    return ((x < y) ? -1 : ((x > y) ? 0 : 1));
-                    else
-                    return ((x > y) ? -1 : ((x < y) ? 0 : 1));
-                });
+    this.navCtrl.setRoot(BrowseProjectsPage, { 'projects': this.PassedFilteredItems, 'filter': item });
+  }
 
 
+  public sortByKey(array, key) {
+    return array.sort(function (a, b) {
+      var x, y;
+      var parts1, parts2, atime, btime;
 
+      if ((key == 'likes') || (key == 'views')) {
+        x = a[key];
+        y = b[key];
+      }
+      else if ((key == 'newest') || (key == 'oldest')) {
+        atime = new Date(a['timestamp']);
+        btime = new Date(b['timestamp']);
+        atime = atime.getFullYear() + "/" + atime.getMonth() + "/" + atime.getDate();
+        btime = btime.getFullYear() + "/" + btime.getMonth() + "/" + btime.getDate();
 
+        parts1 = atime.split("/");
+        parts2 = btime.split("/");
+        y = new Date(parts1[2], parts1[1], parts1[0]);
+        x = new Date(parts2[2], parts2[1], parts2[0]);
 
+      }
+      if (key == 'oldest')
+        return ((x < y) ? -1 : ((x > y) ? 0 : 1));
+      else
+        return ((x > y) ? -1 : ((x < y) ? 0 : 1));
+    });
+
+  }
 }
